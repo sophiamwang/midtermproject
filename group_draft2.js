a7bc4d2b3a2c9ff0758aae90f141ddbb048b3861
sophiamwang <smw603@nyu.edu>
2018-03-26 19:48:49 -0400

    Update group_draft2.js

--- group_draft2.js
+++ group_draft2.js
@@ -254,7 +254,7 @@ class Runner{
 function keyPressed() {
   if (keyCode === 87) {
 	//w = jump = 7
-    for (frames = 0; frames < 30;frames++){
+    for (frames = 0; frames < 130;frames++){
 	 pic = jumpPic;
 	  console.log("loop!!");
     }
